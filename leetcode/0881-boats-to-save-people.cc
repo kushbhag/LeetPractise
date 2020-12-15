e933ffc9c9fc6d5e57798889325e1c59a431795b
Kush Bhagat <bhagat.kush.a@gmail.com>
2020-12-14 22:42:52 -0500

    881

--- leetcode/0881-boats-to-save-people.cc
+++ leetcode/0881-boats-to-save-people.cc
@@ -7,21 +7,6 @@ using namespace std;
 
 class Solution {
     int lo, hi, boats = 0;
-    vector<int> copy;
-    void findAndSetZero(vector<int>& people, int find) {
-        int low = 0, high = size - 1, mid = (low + high) / 2;
-        while (low <= high) {
-            if (copy[mid] > find) {
-                low = mid + 1;
-            } else if (copy[mid] < find) {
-                high = mid - 1;
-            } else if (copy[mid] == find) {
-                people[mid] = 0;
-                break;
-            }
-            mid = (low + high) / 2;
-        }
-    }
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(), people.end(), greater<int> ());
@@ -30,15 +15,7 @@ public:
         while (lo < hi) {
             ++boats;
             if (people[hi] + people[lo] <= limit) --hi;
-        }
-        size = people.size();
-        copy = people;
-        for (int i = 0; i < size; ++i) {
-            if (people[i] != 0) {
-                people[i] = 0;
-                findAndSetZero(people, limit-people[i]);
-                ++boats;
-            }
+            ++lo;
         }
         return boats;
     }
