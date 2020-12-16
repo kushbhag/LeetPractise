6abbe302ab6006e683b53165be0871d55781a275
Kush Bhagat <bhagat.kush.a@gmail.com>
2020-12-16 18:58:08 -0500

    39

--- leetcode/0039-combination-sum.cc
+++ leetcode/0039-combination-sum.cc
@@ -6,28 +6,24 @@
 using namespace std;
 
 class Solution {
+    void combinationSumRec(vector<int>& candidates, int sum, int start, vector<int> temp, vector<vector<int>>& ret) {
+        if (sum < 0) return;
+        else if (sum == 0) {
+            ret.push_back(temp);
+            return;
+        }
+        for (int i = start; i < candidates.size(); ++i) {
+            if (sum - candidates[i] < 0) break;
+            temp.push_back(candidates[i]);
+            combinationSumRec(candidates, sum - candidates[i], i, temp, ret);
+            temp.pop_back();
+        }
+    }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<vector<int>>> v (target+1, vector<vector<int>> (0));
-        v[0] = vector<vector<int>> (1, vector<int> (0));
         sort(candidates.begin(), candidates.end());
-        int size = candidates.size();
-        for (int i = 1; i <= target; ++i) {
-            unordered_set<int> s;
-            for (int j = 0; j < size; ++j) {
-                if (s.find(candidates[j]) != s.end()) continue;
-                if (candidates[j] > i) break;
-                else if (v[i - candidates[j]].size() > 0) {
-                    for (vector<int> temp: v[i - candidates[j]]) {
-                        for (int n: temp) {
-                            s.insert(n);
-                        }
-                        temp.push_back(candidates[j]);
-                        v[i].push_back(temp);
-                    }
-                }
-            }
-        }
-        return v[target];
+        vector<vector<int>> ret;
+        combinationSumRec(candidates, target, 0, vector<int> (), ret);
+        return ret;
     }
 };
