1cccd8b43444ee4c576ff86cc97563ce2f7a9a87
Kush Bhagat <bhagat.kush.a@gmail.com>
2020-12-31 02:41:19 -0500

    91

--- leetcode/0091-decode-ways.cc
+++ leetcode/0091-decode-ways.cc
@@ -2,6 +2,7 @@
 // Medium
 
 #include<unordered_map>
+#include<vector>
 #include<string>
 #include<iostream>
 using namespace std;
@@ -42,8 +43,36 @@ public:
     }
 };
 
+// DP Solution
+class Solution1 {
+public:
+    int numDecodings(string s) {
+        int len = s.length();
+        if (len == 1) return s == "0" ? 0 : 1;
+        vector<int> v (len+1, 0);
+        if(s[len-1] != '0') {
+            v[len-1] = 1;
+        } 
+        v[len] = 1;
+        int count;
+        for (int i = len-2; i >= 0; --i) {
+            count = 0;
+            for (int j = 1; j <= 2; ++j) {
+                string temp = s.substr(i, j);
+                if (temp == "0" || temp == "00") {
+                    count = 0;
+                    break;
+                }
+                if (stoi(temp) <= 26) {
+                    count += v[i+j];
+                }
+            }
+            v[i] = count;
+        }
+        return v[0];
+    }
+};
+
 int main() {
-    string s = "26";
-    string s1 = "27";
-    cout << (s1 > "19") << endl;
+
 }
