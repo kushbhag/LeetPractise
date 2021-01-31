5eba202f6ac63aa79d1c2c3764e43653b9d69432
Kush Bhagat <bhagat.kush.a@gmail.com>
2021-01-30 22:31:13 -0500

    200

--- leetcode/0200-number-of-islands.cc
+++ leetcode/0200-number-of-islands.cc
@@ -6,34 +6,29 @@
 using namespace std;
 
 class Solution {
-    int cols;
-    int rows;
-    void markIsland(vector<vector<char>>& grid, vector<vector<bool>>& memo, int i, int j) {
-        memo[i][j] = true;
-        if (i-1 >= 0 && !memo[i-1][j] && grid[i-1][j] == '1') {
-            markIsland(grid, memo, i-1, j);
+    int width;
+    int height;
+    void markVisited(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= height || j < 0 || j >= width || grid[i][j] == '0') {
+            return;
         }
-        if (i+1 < rows && !memo[i-1][j] && grid[i+1][j] == '1') {
-            markIsland(grid, memo, i+1, j);
-        }
-        if (j-1 >= 0 && !memo[i][j-1] && grid[i][j-1] == '1') {
-            markIsland(grid, memo, i, j-1);
-        }
-        if (j+1 < cols && !memo[i][j+1] && grid[i][j+1] == '1') {
-            markIsland(grid, memo, i, j+1);
-        }
+        grid[i][j] = '0';
+        markVisited(grid, i+1, j);
+        markVisited(grid, i-1,j);
+        markVisited(grid, i, j+1);
+        markVisited(grid, i, j-1);
+
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        cols = grid[0].size();
-        rows = grid.size();
+        width = grid[0].size();
+        height = grid.size();
         int islands = 0;
-        vector<vector<bool>> v (rows, vector<bool> (cols, false));
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (grid[i][j] == '1' && !v[i][j]) {
-                    markIsland(grid, v, i, j);
-                    ++islands;
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    markVisited(grid, i, j);
                 }
             }
         }
