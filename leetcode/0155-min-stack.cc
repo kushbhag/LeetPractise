38f818c9c1c3d207db5d99bbde8bcd616e840f5b
Kush Bhagat <bhagat.kush.a@gmail.com>
2021-01-30 22:30:58 -0500

    155

--- leetcode/0155-min-stack.cc
+++ leetcode/0155-min-stack.cc
@@ -4,87 +4,33 @@
 #include<vector>
 using namespace std;
 
-// SHOULDVE USED TWO VECTORS INSTEAD
-class MinStack2 {
-public:
-    /** initialize your data structure here. */
-    vector<int> v;
-    pair<int, int> p;
-    MinStack2() {
-        p = pair<int, int> (INT_MAX, 0);
-    }
-    
-    void push(int x) {
-        v.push_back(x);
-        if (x < p.first) {
-            p.second = 0;
-            p.first = x;
-        } else {
-            ++p.second;
-        }
-    }
-
-    void pop() {
-        v.pop_back();
-        --p.second;
-        if (p.second <= 0) {
-            p.first = INT_MAX;
-            int size = v.size();
-            for (int i = 0; i < size; ++i) {
-                if (v[i] < p.first) {
-                    p.second = size-i-1;
-                    p.first = v[i];
-                }
-            }
-        }
-
-    }
-
-    int top() {
-        return v.back();
-    }
-
-    int getMin() {
-        return p.first;
-    }
-};
-
-
 class MinStack {
 public:
     /** initialize your data structure here. */
-    vector<int> v;
-    vector<int> mins;
+    vector<int> v1;
+    vector<int> v2;
     MinStack() {
     }
     
     void push(int x) {
-        v.push_back(x);
-        int temp = x;
-        for (int i = 0; i < mins.size(); ++i) {
-            if (temp < mins[i]) {
-                swap(mins[i], temp);
-            }
+        if (v2.empty() || v2.back() >= x) {
+            v2.push_back(x);
         }
-        mins.emplace_back(temp);
+        v1.push_back(x);
     }
 
     void pop() {
-        int popped = v.back();
-        v.pop_back();
-        for (int i = 0; i < mins.size() - 1; ++i) {
-            if (mins[i] == popped) {
-                swap(mins[i], mins[i+1]);
-            }
+        if (v1.back() == v2.back()) {
+            v2.pop_back();
         }
-        mins.pop_back();
+        v1.pop_back();
     }
 
     int top() {
-        return v.back();
+        return v1.back();
     }
 
     int getMin() {
-        return mins.front();
+        return v2.back();
     }
 };
