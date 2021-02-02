3bebc79d292b6f563f05854519aca367f75e5830
Kush Bhagat <bhagat.kush.a@gmail.com>
2021-02-02 12:43:41 -0500

    198

--- leetcode/0198-house-robber.cc
+++ leetcode/0198-house-robber.cc
@@ -8,20 +8,11 @@ using namespace std;
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int maxSoFar = 0, size = nums.size();
-        if (size == 0) return 0;
-        int first, second, firstPrev = 0, firstPrevPrev = 0, secondPrev = 0, secondPrevPrev = 0;
-
-        first = max(firstPrev, nums[0] + firstPrevPrev);
-        maxSoFar = max(maxSoFar, first);
+        int size = nums.size();
+        vector<int> mem (size+2, 0);
         for (int i = 0; i < size; ++i) {
-            first= max(firstPrev, nums[i] + firstPrevPrev);
-            second = max(secondPrev, nums[i] + secondPrevPrev);
-            firstPrevPrev = firstPrev;
-            firstPrev = first;
-            secondPrevPrev = secondPrev;
-            secondPrev = second;
+            mem[i+2] = max(nums[i] + mem[i], mem[i+1]);
         }
-        return max(first, second);
+        return mem[size+1];
     }
 };
