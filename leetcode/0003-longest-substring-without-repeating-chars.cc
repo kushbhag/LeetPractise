af0850db84d64de2e12648398a16eaa560584aa3
Kush Bhagat <bhagat.kush.a@gmail.com>
2021-08-19 10:35:22 -0400

    3

--- leetcode/0003-longest-substring-without-repeating-chars.cc
+++ leetcode/0003-longest-substring-without-repeating-chars.cc
@@ -7,26 +7,19 @@
 using namespace std;
 
 class Solution {
-    int maxSoFar = 0, low = 0, hi = 0, size, tempMax;
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<bool> alpha (96, false);
-        size = s.length();
-        while (hi < size) {
-            if (alpha[s[hi] - ' ']) {
-                if (low == hi) {
-                    ++hi;
-                } else {
-                    alpha[s[low] - ' '] = false;
-                    ++low;
-                    --tempMax;
-                }
-            } else {
-                alpha[s[hi] - ' '] = true;
-                ++tempMax;
-                ++hi;
-                maxSoFar = tempMax > maxSoFar ? tempMax : maxSoFar;
+        int len = s.length();
+        int lo = 0, hi = 0, maxSoFar = 0;
+        int a [128] = {0};
+        while(hi < len) {
+            a[s[hi]]++;
+            while (a[s[hi]] > 1) {
+                a[s[lo]]--;
+                lo++;
             }
+            maxSoFar = max(maxSoFar, hi-lo+1);
+            ++hi;
         }
         return maxSoFar;
     }
