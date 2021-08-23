f0084bbe20350b9a9060d89146a78787adb427e6
Kush Bhagat <bhagat.kush.a@gmail.com>
2021-08-22 23:18:51 -0400

    62

--- leetcode/0062-unique-paths.cc
+++ leetcode/0062-unique-paths.cc
@@ -39,3 +39,17 @@ public:
         return v[n-1][m-1];
     }
 };
+
+// Forward Tracking O(n) space
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> pre(n, 1);
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                pre[j] += pre[j-1];
+            }
+        }
+        return pre[n-1];
+    }
+};
