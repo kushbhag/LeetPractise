760d7f94d497dd47364dec1b8038ebfa67bc4012
Kush Bhagat <bhagat.kush.a@gmail.com>
2022-01-20 23:09:27 -0500

    1

---
+++ leetcode/python/0001-two-sum.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/two-sum/
+# Easy
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        s = {}
+        for i, n in enumerate(nums):
+            if target-n in s:
+                return [s[target-n], i]
+            s[n] = i
+        return []
+        
