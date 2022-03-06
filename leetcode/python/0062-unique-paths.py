4c1ae15d7f77ff923f5b3f925c6dc0aa039f7d15
Kush Bhagat <bhagat.kush.a@gmail.com>
2022-03-06 13:32:17 -0500

    62

---
+++ leetcode/python/0062-unique-paths.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/unique-paths/
+# Medium
+
+# 2D DP
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1]*n
+
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j]+dp[j-1]
+        return dp[-1]
