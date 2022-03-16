a3ce2f35148cba2af516bbcecd705cc26e146c86
Kush Bhagat <bhagat.kush.a@gmail.com>
2022-03-16 19:24:01 -0400

    207

--- leetcode/python/0207-course-schedule.py
+++ leetcode/python/0207-course-schedule.py
@@ -1,5 +1,39 @@
 # https://leetcode.com/problems/course-schedule/
 # Medium
+# TWO Solutions
+
+# BFS that counts the indegree for each vertex
+# Start only on the nodes with indegree == 0 (leaf nodes)
+# Then parse through it's neighbors, reducing the indegree by 1
+# Then continuously add the neighboring nodes only if the indegree of them is 0
+#      We do this so we don't have duplicates nodes in the queue
+# If ever the indegree of the neighbor is less than 0 there is a cycle (return False)
+# Once the BFS is complete, we `return not any(g)`
+# Any checks if there is anything in the iterator that is True
+# So if all elements are zero, not any() will return True
+# We can technically just return True over here, but it will fail when we have self cycle [[0,1], [1, 1]]
+from collections import deque
+class Solution:
+    def canFinish(self, n: int, prereq: List[List[int]]) -> bool:
+        graph = [[] for _ in range(n)]
+        g = [0] * n
+        
+        for f, s in prereq:
+            graph[f].append(s)
+            g[s] += 1
+        
+        q = deque([p for p in range(n) if g[p] == 0])
+        while q:
+            p = q.popleft()
+            for neighbor in graph[p]:
+                g[neighbor] -= 1
+                if g[neighbor] == 0:
+                    q.append(neighbor)
+                elif g[neighbor] < 0:
+                    return False
+        return not any(g)
+                
+
 
 # It is NOT just a simple DFS
 # You also need to check if you have fully parsed the course you are currently at before or not
